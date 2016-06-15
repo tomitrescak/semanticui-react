1961d1401f6ee50d005c87c782ed5b7960de592f
Tomas Trescak <tomi.trescak@gmail.com>
2016-06-16 08:56:03 +1000

    Possibility to configure anchor elements

---
+++ src/config/interfaces.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+declare global {
+  interface IColor {
+    color?: "warning" | "info" | "success" | "error" | "blue" | "red" | "olive" | "orange" | "yellow" | "green" | "teal" | "blue" | "violet" | "pink" | "purple" | "brown" | "grey" | "black";
+  }
+}
